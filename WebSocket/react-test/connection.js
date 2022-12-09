6e1606dbb3f4585c6dee0d17bd01ed12620eebdb
Andrés Morales <mora200217@hotmail.com>
2022-12-08 23:18:39 -0500

    [Update] Image for disconnection advice

---
+++ WebSocket/react-test/connection.js
@@ -0,0 +1,5 @@
+const ws = new WebSocket("wss://demo.piesocket.com/v3/channel_123?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self")
+
+ws.onopen((e)=>{
+    alert(e)
+})
--- src/GUI/ConnectionIndicator.js
+++ src/GUI/ConnectionIndicator.js
@@ -1,11 +1,11 @@
 // connectionIndicator.js 
 import './styles/connectionIndicator.css'
-
+import Logo from './assets/leap.svg'; 
 export const ConnectionIndicator = () => {
     return(
         <div className = "modal-main" id = "modal-leap">
             <div className = "d-flex flex-column justify-content-center align-items-center h-100">
-                {/* <img> </img> */}
+                <img  className = "logo-leap" src = { Logo } />
                 <p style = { {color: 'white'}}>Connect the <strong> LeapMotion</strong> or Unpause it to continue.</p>
             </div>
         </div>
